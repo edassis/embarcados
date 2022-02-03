ede8cccc7c23f2333e04c3ac58fd7d2a7f8dcbda
Eduardo Assis <eduardoffassis@gmail.com>
2022-02-03 18:02:12 -0300

    Initial commit
    Record video and opens it on default app

---
+++ app/src/main/java/com/example/embarcados/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.embarcados;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    // Define the pic id
+    private static final int pic_id = 157;
+
+    // Define the button and imageview type variable
+    Button camera_open_id;
+    ImageView click_image_id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // By ID we can get each component
+        // which id is assigned in XML file
+        // get Buttons and imageview.
+        camera_open_id = (Button)findViewById(R.id.camera_button);
+        click_image_id = (ImageView)findViewById(R.id.click_image);
+
+        // Camera_open button is for open the camera
+        // and add the setOnClickListener in this button
+        camera_open_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create the camera_intent ACTION_IMAGE_CAPTURE
+                // it will open the camera for capture the image
+                Intent camera_intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+                // Start the activity with camera_intent,
+                // and request pic id
+                startActivityForResult(camera_intent, pic_id);
+            }
+        });
+    }
+
+    // This method will help to retrieve the image
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Match the request 'pic id with requestCode
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("D","FON " + String.valueOf(requestCode));
+        Log.d("D","FON2 " + String.valueOf(data));
+
+        Uri vid = data.getData();
+
+        Intent openVid = new Intent(Intent.ACTION_VIEW);
+        openVid.setDataAndType(vid, "video/*");
+        openVid.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);//DO NOT FORGET THIS EVER
+        startActivity(openVid);
+
+        if (requestCode == pic_id) {
+//            // BitMap is data structure of image file
+//            // which stor the image in memory
+//            Bitmap photo = (Bitmap) data.getExtras().get("data");
+//
+//            // Set the image in imageview for display
+//            click_image_id.setImageBitmap(photo);
+        }
+    }
+}
